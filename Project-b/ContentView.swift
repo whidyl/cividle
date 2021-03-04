600fe9665beab59255efbc22ca8db16fdf229b4c
whidyl <whidyldev@gmail.com>
2021-03-04 13:37:31 -0700

    Initial Commit

---
+++ Project-b/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-b
+//
+//  Created by Dylan Whitehurst on 3/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-b/Project_bApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_bApp.swift
+//  Project-b
+//
+//  Created by Dylan Whitehurst on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Project_bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
